90cc7652c52f6ab9901e5e0e2aca209e03ff5561
Anuj <kumaranuj1303@gmail.com>
2022-04-03 11:57:28 +0530

    change deploy.yml file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get('/', async(req, res) => {
-    res.send('Are we in a mess');
+    res.send('First Iteration');
 })
 
 app.get('/home', async(req, res) => {
